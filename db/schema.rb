79f591614e88c8caee54cc8c30e9fb462caf5397
valmy <valmyericles@gmail.com>
2020-11-06 12:58:26 -0300

    model requisitos de sistema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_105601) do
+ActiveRecord::Schema.define(version: 2020_11_06_143341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "system_requirements", force: :cascade do |t|
+    t.string "name"
+    t.string "operational_system"
+    t.string "storage"
+    t.string "processor"
+    t.string "memory"
+    t.string "video_board"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,3 @@
-Rspec.configure do |config|
-  config.include FactoryBot::Sintax::Methods
-end
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end 
