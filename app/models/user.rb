7fa20fdc02479ea8793e3573468c84d427dad1c5
valmy <valmyericles@gmail.com>
2020-11-07 11:34:02 -0300

    melhorando model user devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :name, :profile, presence: true
+
+  enum profile: { admin: 0, client: 1 }
 end
---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { "12345678" }
+    password_confirmation { "12345678" }
+    profile { %i(admin client).sample }
+  end
+end
