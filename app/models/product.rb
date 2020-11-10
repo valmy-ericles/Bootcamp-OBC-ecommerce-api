2016d961f66957b43b465885e367cf8e4112c71b
valmy <valmyericles@gmail.com>
2020-11-10 07:44:05 -0300

    add image to model and factorie

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+
+  has_one_attached :image
+  validates :image, presence: true
 end
