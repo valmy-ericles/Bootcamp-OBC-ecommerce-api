6331f2d4c971ce6023b108c37995ea2c9ae8a21f
valmy <valmyericles@gmail.com>
2020-11-14 10:56:33 -0300

    config de restriçao de acesso

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -1,13 +1,27 @@
 module Admin
   module V1
     class ApiController < ApplicationController
+      class ForbiddenAccess < StandardError; end
+
       include Authenticable
+
+      before_action :restrict_access_for_admin!
 
       def render_errors(message: nil, fields: nil, status: :unprocessable_entity)
         errors = {}
         errors['fields'] = fields if fields.present?
         errors['message'] = message if message.present?
         render json: { errors: errors }, status: status
+      end
+
+      rescue_from ForbiddenAccess do
+        render_errors(message: 'Forbidden access', status: :forbidden)
+      end
+
+      private
+
+      def restrict_access_for_admin!
+        raise ForbiddenAccess unless current_user.admin?
       end
     end
   end
