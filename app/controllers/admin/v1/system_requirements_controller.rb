84c3e84b9d35d298fd9073355831e2fa0365f0d9
valmy <valmyericles@gmail.com>
2020-11-17 12:41:27 -0300

    admin requests system requirements ok

---
+++ app/controllers/admin/v1/system_requirements_controller.rb
@@ -0,0 +1,51 @@
+module Admin
+  module V1
+    class SystemRequirementsController < ApiController
+      before_action :load_system_requirement, only: %i[update destroy]
+
+      def index
+        @system_requirements = SystemRequirement.all
+      end
+
+      def create
+        @system_requirement = SystemRequirement.new
+        @system_requirement.attributes = system_requirement_params
+        save_system_requirement!
+      end
+
+      def update
+        @system_requirement.attributes = system_requirement_params
+        save_system_requirement!
+      end
+
+      def destroy
+        @system_requirement.destroy!
+      rescue StandardError
+        render_errors(fields: @system_requirement.errors.messages)
+      end
+
+      private
+
+      def load_system_requirement
+        @system_requirement = SystemRequirement.find(params[:id])
+      end
+
+      def system_requirement_params
+        return {} unless params.key?(:system_requirement)
+
+        params.require(:system_requirement).permit(
+          :name, :operational_system, :storage,
+          :processor, :memory, :video_board
+        )
+      end
+
+      def save_system_requirement!
+        @system_requirement.save!
+        render :show
+      rescue StandardError
+        render_errors(fields: @system_requirement.errors.messages)
+      end
+    end
+  end
+end
+
---
+++ app/views/admin/v1/system_requirements/show.json.jbuilder
@@ -0,0 +1,3 @@
+json.system_requirement do
+  json.call(@system_requirement, :id, :name, :operational_system, :storage, :processor, :memory, :video_board)
+end
