57e9b9bb8dc0dbaad969a6de538c1e9b22f4e24e
valmy <valmyericles@gmail.com>
2020-11-30 11:43:13 -0300

    refactor model_loading_service_spec

--- spec/services/admin/model_loading_service_spec.rb
+++ spec/services/admin/model_loading_service_spec.rb
@@ -21,12 +21,36 @@ describe Admin::ModelLoadingService do
         expect(service.records.count).to eq 4
       end
 
-      it 'returns records following search, order and pagination' do
+      it 'do right search, order and pagination' do
         search_categories.sort! { |a, b| b[:name] <=> a[:name] }
         service = described_class.new(Category.all, params)
         service.call
         expected_categories = search_categories[4..7]
         expect(service.records).to contain_exactly(*expected_categories)
+      end
+
+      it 'sets right :page' do
+        service = described_class.new(Category.all, params)
+        service.call
+        expect(service.pagination[:page]).to eq 2
+      end
+
+      it 'sets right :length' do
+        service = described_class.new(Category.all, params)
+        service.call
+        expect(service.pagination[:length]).to eq 4
+      end
+
+      it 'sets right :total' do
+        service = described_class.new(Category.all, params)
+        service.call
+        expect(service.pagination[:total]).to eq 15
+      end
+
+      it 'sets right :total_pages' do
+        service = described_class.new(Category.all, params)
+        service.call
+        expect(service.pagination[:total_pages]).to eq 4
       end
     end
 
@@ -42,6 +66,30 @@ describe Admin::ModelLoadingService do
         service.call
         expected_categories = categories[0..9]
         expect(service.records).to contain_exactly(*expected_categories)
+      end
+
+      it 'sets right :page' do
+        service = described_class.new(Category.all, nil)
+        service.call
+        expect(service.pagination[:page]).to eq 1
+      end
+
+      it 'sets right :length' do
+        service = described_class.new(Category.all, nil)
+        service.call
+        expect(service.pagination[:length]).to eq 10
+      end
+
+      it 'sets right :total' do
+        service = described_class.new(Category.all, nil)
+        service.call
+        expect(service.pagination[:total]).to eq 15
+      end
+
+      it 'sets right :total_pages' do
+        service = described_class.new(Category.all, nil)
+        service.call
+        expect(service.pagination[:total_pages]).to eq 2
       end
     end
   end
