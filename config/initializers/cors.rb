62c8bc3c41cf7547fc664049c66c8471e928bdd9
valmy <valmyericles@gmail.com>
2020-12-01 16:56:26 -0300

    expose cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,5 +5,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      expose: ['access-token', 'client', 'expiry', 'uid']
   end
 end
